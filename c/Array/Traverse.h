6d19c6d0f123c292f81c81b1922023f1fe18b484
Pravin Adhav <pravinadhav4945@gmail.com>
2022-06-29 15:05:17 +0530

    function defination for traversing array.

---
+++ c/Array/Traverse.h
@@ -0,0 +1,11 @@
+// time complexity of traversal of array is O(n).
+void traverse(int arr[],int track)
+{
+    int i=0;
+    printf("\ntraversing the array \nelements : ");
+    for(i;i<track;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
