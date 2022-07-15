1b4363cccf0d4b77486d92f330912bd20cb08731
Pravin Adhav <pravinadhav4945@gmail.com>
2022-07-15 20:44:18 +0530

    optimized bubblesort in c++

--- c++/Sorting/Bubblesort.cpp
+++ c++/Sorting/Bubblesort.cpp
@@ -1,35 +1,42 @@
-#include<iostream>
-void bublesort(int *a,int size);//function to sort the array
-void swap(int *a,int *b);//function to swap the elements
-void display(int *a,int size);//function to display the array
+#include <iostream>
+void bublesort(int *a, int size); // function to sort the array
+void swap(int *a, int *b);        // function to swap the elements
+void display(int *a, int size);   // function to display the array
 int main()
 {
-    int arr[] = {10,5,6,3,7,8,2};
-    int size=sizeof(arr)/sizeof(int);//size of array
+    int arr[] = {10, 5, 6, 3, 7, 8, 2};
+    int size = sizeof(arr) / sizeof(int); // size of array
     printf("\nBefore sorting : ");
-    display(arr,size);
-    bublesort(arr,size);//calling function to sort the array
+    display(arr, size);
+    bublesort(arr, size); // calling function to sort the array
     printf("\nAfter sorting : ");
-    display(arr,size);
+    display(arr, size);
     return 0;
 }
-void bublesort(int *a,int size)
+void bublesort(int *a, int size)
 {
-    int i=0,j=0;
-    for(i=0;i<size;i++)
-    for(j=0;j<size-1-i;j++)
-    if(a[j]>a[j+1])//ascending order
-    // if(a[j]<a[j+1])//descending order
-    swap(&a[j],&a[j+1]);//passing address of elements to swap function
+    int i = 0, j = 0,sorted=0;
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size - 1 - i; j++)
+        {
+            if (a[j] > a[j + 1]) // ascending order
+                // if(a[j]<a[j+1])//descending order
+                swap(&a[j], &a[j + 1]); // passing address of elements to swap function
+                sorted=1;
+        }
+        if(sorted==0)
+            break;
+    }
 }
-void swap(int *a,int *b)
+void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-void display(int *a,int size)
+void display(int *a, int size)
 {
-    for(int i=0;i<size;i++)
-    printf(" %d",a[i]);
+    for (int i = 0; i < size; i++)
+        printf(" %d", a[i]);
 }
